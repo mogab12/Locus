f7d896e4af3ba91545939afe19af2fde658da207
Gabriel Cordeiro Caram <gabrielccaram@usp.br>
2024-11-15 18:09:10 -0300

    Alterando permissões do administrador

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'user_type', 'curso', 'semestre', 'is_staff']
+    list_filter = ['user_type', 'is_staff', 'is_superuser']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações Adicionais', {'fields': ('user_type', 'curso', 'semestre')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Informações Adicionais', {'fields': ('user_type', 'curso', 'semestre')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
