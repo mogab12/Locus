b0195076a1af7d2290d18daa501dcfbd9bfbf674
Gabriel Cordeiro Caram <gabrielccaram@usp.br>
2024-11-15 16:45:00 -0300

    Adicionando Sistema de Login

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('disciplinas/', views.disciplinas, name='disciplinas'),
     path('eventos/', views.eventos, name='eventos'),
     path('notificacoes/', views.notificacoes, name='notificacoes'),
