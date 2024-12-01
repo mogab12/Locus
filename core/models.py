7a4df883a4f38902b85f69f542db3d56db584304
Gabriel Cordeiro Caram <gabrielccaram@usp.br>
2024-12-01 13:01:11 -0300

    Adicionar dados de prédios e salas

--- core/models.py
+++ core/models.py
@@ -123,3 +123,20 @@ class HorarioGrade(models.Model):
 
     def __str__(self):
         return f"{self.usuario} - {self.disciplina.nome} ({self.get_dia_da_semana_display()})"
+    
+
+class Predio(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.nome
+    
+
+class Sala(models.Model):
+    nome = models.CharField(max_length=50)
+    predio = models.ForeignKey(Predio, on_delete=models.CASCADE, related_name='salas')
+
+    def __str__(self):
+        return f"{self.nome} ({self.predio.nome})"
